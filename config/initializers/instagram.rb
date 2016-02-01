eb092086f000d202193018f8a1d50a912a3296bb
hidehikonakajima <hide_28soba@bk2.so-net.ne.jp>
2016-02-01 05:47:10 +0000

    first config

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+require "instagram"
+    Instagram.configure do |config|
+    config.client_id = "13adc27e71a340f5979adf1eeea77fe0"
+    config.client_secret = "9d824cc322104f25818a47dc4d9727a4"
+end
